f95c40debcc0823fd677699e6050dea30a38ab14
xholjan <xholjan.malia@gmail.com>
2026-03-19 21:26:41 +0100

    Fixed last 2 warnings

--- Infrastructure/Services/EmailService.cs
+++ Infrastructure/Services/EmailService.cs
@@ -1,4 +1,5 @@
-﻿using Application.Interfaces;
+﻿using Application.Exceptions;
+using Application.Interfaces;
 using Microsoft.Extensions.Configuration;
 using System.Net;
 using System.Net.Mail;
@@ -16,28 +17,26 @@ namespace Infrastructure.Services
 
         public async Task SendEmailAsync(string to, string subject, string body)
         {
-            try
+            var smtpHost = _configuration["Email:SmtpHost"];
+            var smtpPort = int.Parse(_configuration["Email:SmtpPort"]!);
+            var username = _configuration["Email:Username"];
+            var password = _configuration["Email:Password"];
+            var from = _configuration["Email:From"];
+
+            if (string.IsNullOrEmpty(smtpHost) || string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password) || string.IsNullOrEmpty(from))
             {
-                var smtpHost = _configuration["Email:SmtpHost"];
-                var smtpPort = int.Parse(_configuration["Email:SmtpPort"]!);
-                var username = _configuration["Email:Username"];
-                var password = _configuration["Email:Password"];
-                var from = _configuration["Email:From"];
-
-                using var client = new SmtpClient(smtpHost, smtpPort)
-                {
-                    Credentials = new NetworkCredential(username, password),
-                    EnableSsl = true
-                };
-
-                var mail = new MailMessage(from, to, subject, body);
-
-                await client.SendMailAsync(mail);
+                throw new CustomException("Email configuration is missing.");
             }
-            catch (Exception ex)
+
+            using var client = new SmtpClient(smtpHost, smtpPort)
             {
-                throw;
-            }
+                Credentials = new NetworkCredential(username, password),
+                EnableSsl = true
+            };
+
+            var mail = new MailMessage(from, to, subject, body);
+
+            await client.SendMailAsync(mail);
         }
     }
 }
